c6a9d326f815244313f87554f7e32a85d02a4352
Dario Salvi <dario.salvi@eng.ox.ac.uk>
2015-11-05 18:11:25 +0000

    refactored javacript interface from example code

---
+++ www/sensorlogger.js
@@ -0,0 +1,20 @@
+var exec = require("cordova/exec");
+
+var SensorLogger = function () {
+    this.name = "SensorLogger";
+};
+
+SensorLogger.prototype.init = function (sensors, onSuccess, onError) {
+    exec(onSuccess, onError, "SensorLogger", "init", [sensors]);
+};
+
+SensorLogger.prototype.start = function (onSuccess, onError) {
+    exec(onSuccess, onError, "SensorLogger", "start", []);
+};
+
+SensorLogger.prototype.stop = function (onSuccess, onError) {
+    exec(onSuccess, onError, "SensorLogger", "stop", []);
+};
+
+
+module.exports = new SensorLogger();
